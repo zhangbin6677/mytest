b71182f7b59fe6de9eca3ceec9319d80ab068cbc
zhangbin <123456@qq.com>
2018-04-10 20:37:09 +0800

    第三次

--- demo-test/src/main/java/cn/itcast/demo/Test.java
+++ demo-test/src/main/java/cn/itcast/demo/Test.java
@@ -10,6 +10,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("========1111========");
+		System.out.println("========2222========");
+		System.out.println("========3333========");
+		System.out.println("========4444========");
 	}
 
 }
